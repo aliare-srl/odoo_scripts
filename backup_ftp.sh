fcb385eb52fcff72023765e58f6a421440258090
ASR <sebarolando@aliare.com.ar>
2023-01-18 12:20:27 -0300

    Primer intento del archivo backup_ftp.sh

---
+++ backup_ftp.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+servidorftp="mochipa.com.ar"
+userftp=backupcliente
+passftp=backupcliente
+
+origen=/var/log/odoo/backup
+destino=/Odoo/full24backup
+
+ftpurl="ftp://$userftp:$passftp@$servidorftp"
+
+#eliminarantiguos="--delete"
+
+lftp -c <<EOF
+set ftp:list-options -a;
+open '$ftpurl'
+lcd $origen
+cd $destion
+mirror --reverse \
+       	$eliminarantiguos \
+		--verbose
+EOF

57f66e55b3ae8d1f95fe4b3b807dafaeab039fa1
ASR <sebarolando@aliare.com.ar>
2023-01-18 12:43:47 -0300

    Ahora elimina los archivos antiguos, solo quedan lo inficado en la variable conservar

--- backup_ftp.sh
+++ backup_ftp.sh
@@ -5,11 +5,19 @@ userftp=backupcliente
 passftp=backupcliente
 
 origen=/var/log/odoo/backup
+# Full path en el servidor ftp. La carpeta debe existir
 destino=/Odoo/full24backup
 
 ftpurl="ftp://$userftp:$passftp@$servidorftp"
 
-#eliminarantiguos="--delete"
+eliminarantiguos="--delete"
+
+# Copias a conservar 
+conservar=5
+((conservar=conservar+1)) # necesario para saltear n archivos, hay que especificar n + 1
+
+#Elimina los archivos mas viejos, dejando solo los indicados en la variable conservar
+ls -tp | grep -v '\$' | tail -n +$conservar | xargs -I {} -d '\n' -r rm -- {}
 
 lftp -c "set ftp:list-options -a;
 open '$ftpurl';
